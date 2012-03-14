3266fd86f38ae5482ea411ac2b6cee9a563d4d74
eternalmatt@gmail.com <eternalmatt@gmail.com>
2012-03-13 23:48:18 -0400

    Initial Android project

---
+++ src/edu/uncc/cci/KnightVisor/KnightVisorActivity.java
@@ -0,0 +1,13 @@
+package edu.uncc.cci.KnightVisor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class KnightVisorActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
