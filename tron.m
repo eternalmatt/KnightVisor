528e7660f341350ab15c5fce4b53d977d63fd72f
eternalmatt@gmail.com <eternalmatt@gmail.com>
2012-03-14 20:38:15 -0400

    Threshold edges before applying outline

--- tron.m
+++ tron.m
@@ -1,6 +1,7 @@
 function [ g ] = tron ( f )
 
 edges = sobel(f);
+edges = threshold(edges);
 
 [R,C] = size(f);
 g = uint8(zeros(R,C,3));
@@ -9,7 +10,7 @@ for r = 1:R
 for c = 1:C
   g(r,c,:) = f(r,c);
   
-  if (edges(r,c) == 255)
+  if (edges(r,c) == 1)
     g(r,c,2) = 255;
   end
 
