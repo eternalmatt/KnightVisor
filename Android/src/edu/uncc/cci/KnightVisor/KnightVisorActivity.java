cb75764a7d89aab854a9fa9574254d2afa7fcfd2
eternalmatt@gmail.com <eternalmatt@gmail.com>
2012-03-26 20:43:33 -0400

    Main activity with very basic camera view

--- Android/src/edu/uncc/cci/KnightVisor/KnightVisorActivity.java
+++ Android/src/edu/uncc/cci/KnightVisor/KnightVisorActivity.java
@@ -1,13 +1,84 @@
 package edu.uncc.cci.KnightVisor;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.io.IOException;
 
-public class KnightVisorActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+import android.app.Activity;
+import android.hardware.Camera;
+import android.media.SoundPool;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+public class KnightVisorActivity extends Activity implements SurfaceHolder.Callback {
+    
+	private Camera camera;
+	private SurfaceView cameraView;
+	private FrameLayout frameLayout;
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Get the entire screen
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		// Create the surface for the camera to draw its preview on
+		cameraView = new SurfaceView(this);
+		cameraView.getHolder().setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+		cameraView.getHolder().addCallback(this);
+
+		// Setup the layout where the cameraView is completely obscured by the edgeView
+		frameLayout = new FrameLayout(this);
+		frameLayout.addView(cameraView);
+		setContentView(frameLayout);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		stopCameraPreview();
+	}
+
+	private void stopCameraPreview() {
+		if (camera != null) {
+			camera.setPreviewCallback(null);
+			camera.stopPreview();
+			camera.release();
+			camera = null;
+		}
+	}
+
+	private void startCameraPreview() {
+		try {
+			camera = Camera.open();
+			camera.setPreviewDisplay(cameraView.getHolder());
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+			Toast.makeText(this, "Camera is not available", Toast.LENGTH_LONG).show();
+		}
+	}
+
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+		if (camera != null)
+		{
+			Camera.Parameters parameters = camera.getParameters();
+			parameters.setPreviewSize(width, height); // TODO: check that width, height are a valid camera preview size
+			camera.setParameters(parameters);
+			camera.startPreview();
+		}
+	}
+
+	public void surfaceCreated(SurfaceHolder holder) {
+		startCameraPreview();
+	}
+
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		stopCameraPreview();
+	}
 }
