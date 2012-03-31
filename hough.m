211cb43bc07ea2436d1750eb1824d43d5f5cf050
eternalmatt@gmail.com <eternalmatt@gmail.com>
2012-03-31 19:47:00 -0400

    Typo in formula for rho

--- hough.m
+++ hough.m
@@ -7,17 +7,20 @@ tv = [-pi/2 : pi/200 : pi/2];  %thetaVector
 rho0 = 0;                      %minimum rho
 rho1 = floor(sqrt(R^2 + C^2)); %maximum rho (length of diagonal)
 
-%accum is zeros(length of row, length of column)
+%accumulator is zeros(length of row, length of column)
 accum = zeros(numel(tv),rho1); 
 
 for p = points  %foreach p in points
   x = p(1);
   y = p(2);
-  rhov = x + cos(tv) + y * sin(tv);
+  rhov = x * cos(tv) + y * sin(tv);
   for j = 1:numel(tv)
+
+    %if value in rhoVector is valid,
+    %   increment the corresponding cell in accum
     if rhov(j) >= rho0 && rhov(j) <= rho1
-      index = rhov(j) - rho0 + 1;
-      accum(index,j) = accum(index,j) + 1;
+      i = floor(rhov(j) - rho0 + 1);
+      accum(i,j) = accum(i,j) + 1;
     end
   end
 end
