2f88908db523e2e2b0cb223495b683f96373b2f7
eternalmatt@gmail.com <eternalmatt@gmail.com>
2012-04-09 16:48:39 -0400

    Added submodule from github.com/bwr/edgefinder
    Also shuffled a lot of code around in KnightVisorActivity.
    Logic remains the same everywhere, but code is slightly
    more clean.

--- Android/src/edu/uncc/cci/KnightVisor/KnightVisorActivity.java
+++ Android/src/edu/uncc/cci/KnightVisor/KnightVisorActivity.java
@@ -10,23 +10,64 @@ import android.widget.Toast;
 
 public class KnightVisorActivity extends Activity {
 
-    private SurfaceView preview = null;
-    private SurfaceHolder previewHolder = null;
-    private Camera camera = null;
-    private boolean inPreview = false;
-    private boolean cameraConfigured = false;
+    private Camera camera                   = null;
+    private SurfaceHolder surfaceHolder     = null;
+    
+    private boolean inPreview               = false;
+    private boolean cameraConfigured        = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        preview = (SurfaceView) findViewById(R.id.preview);
-        previewHolder = preview.getHolder();
-        previewHolder.addCallback(surfaceCallback);
-        previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        SurfaceView mainScreen = (SurfaceView) findViewById(R.id.preview);
+        surfaceHolder = mainScreen.getHolder();
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        surfaceHolder.addCallback(new SurfaceHolder.Callback() {
+            public void surfaceDestroyed(SurfaceHolder holder) { }
+            public void surfaceCreated  (SurfaceHolder holder) { }
+            public void surfaceChanged  (SurfaceHolder holder, int format, int width, int height) {
+                initPreview(width, height);
+                startPreview();
+            }
+        });
+    }    
+    
+    /* set up camera and configure with proper width/height */
+    private void initPreview(int width, int height) {
+        if (camera != null && surfaceHolder.getSurface() != null) {
+            try {
+                camera.setPreviewDisplay(surfaceHolder);
+            } catch (Throwable t) {
+                Log.e("PreviewDemo-surfaceCallback", "Exception in setPreviewDisplay()", t);
+                Toast.makeText(this, t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+
+            if (!cameraConfigured) {
+                Camera.Parameters parameters = camera.getParameters();
+                Camera.Size size = getBestPreviewSize(width, height, parameters);
+
+                if (size != null) {
+                    parameters.setPreviewSize(size.width, size.height);
+                    camera.setParameters(parameters);
+                    cameraConfigured = true;
+                }
+            }
+        }
+    }
+    
+    /* start preview as long as the camera is configured */
+    private void startPreview() {
+        if (cameraConfigured && camera != null) {
+            camera.startPreview();
+            inPreview = true;
+        }
     }
 
+    
+    /* life cycle functions taking care of camera */
+    
     @Override
     public void onResume() {
         super.onResume();
@@ -48,6 +89,8 @@ public class KnightVisorActivity extends Activity {
         super.onPause();
     }
 
+    
+    /* helper function to set up the display */
     private Camera.Size getBestPreviewSize(int width, int height, Camera.Parameters parameters) {
         Camera.Size result = null;
 
@@ -61,48 +104,4 @@ public class KnightVisorActivity extends Activity {
 
         return result;
     }
-
-    private void initPreview(int width, int height) {
-        if (camera != null && previewHolder.getSurface() != null) {
-            try {
-                camera.setPreviewDisplay(previewHolder);
-            } catch (Throwable t) {
-                Log.e("PreviewDemo-surfaceCallback", "Exception in setPreviewDisplay()", t);
-                Toast.makeText(this, t.getMessage(), Toast.LENGTH_LONG).show();
-            }
-
-            if (!cameraConfigured) {
-                Camera.Parameters parameters = camera.getParameters();
-                Camera.Size size = getBestPreviewSize(width, height, parameters);
-
-                if (size != null) {
-                    parameters.setPreviewSize(size.width, size.height);
-                    camera.setParameters(parameters);
-                    cameraConfigured = true;
-                }
-            }
-        }
-    }
-
-    private void startPreview() {
-        if (cameraConfigured && camera != null) {
-            camera.startPreview();
-            inPreview = true;
-        }
-    }
-
-    SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
-        public void surfaceCreated(SurfaceHolder holder) { /* wait until
-                                                            * surfaceChanged */
-        }
-
-        public void surfaceDestroyed(SurfaceHolder holder) { /* do nothing */
-        }
-
-        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-            initPreview(width, height);
-            startPreview();
-        }
-
-    };
 }
