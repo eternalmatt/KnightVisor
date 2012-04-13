719172b47c8a2f8967533f0c1d59b5fa8899dd28
eternalmatt@gmail.com <eternalmatt@gmail.com>
2012-04-13 02:20:25 -0400

    Removed old code

--- Android/jni/native.c
+++ Android/jni/native.c
@@ -8,10 +8,9 @@ JNIEXPORT void JNICALL Java_edu_uncc_cci_KnightVisor_EdgeView_nativeProcessing
     (JNIEnv *env, jobject this,
      jbyteArray frame, jint width, jint height, jobject buffer)
 {
-	jint  *g = (jint*)((*env)->GetDirectBufferAddress(env, buffer));
 	jbyte *f = (jbyte*)(*env)->GetByteArrayElements(env, frame, 0);
+	jint  *g = (jint*)((*env)->GetDirectBufferAddress(env, buffer));
     const int w = width, maxx = width - 1, maxy = height - 1, length = width * height - width - 1;
-    int y;
     
 #define n11 (f[p-w-1])
 #define n12 (f[p-w  ])
@@ -31,53 +30,19 @@ JNIEXPORT void JNICALL Java_edu_uncc_cci_KnightVisor_EdgeView_nativeProcessing
      n21 n22 n23
      n31 n32 n33
      
-     see, it looks pretty. just don't start using "n11" in other variable names...
-     */
+     see, it looks pretty. just don't start using "n11" in other variable names...*/
     int p;
-    #pragma omp parallel for default(shared) private(p)
+    #pragma omp parallel for default(shared) private(p) schedule(static)
     for (p = width + 1; p < length; p++)
     {
-        int px, py, ps;
-        px = n13 + (n23 << 1) + n33 - (n11 + (n21 << 1) + n31);
-        //previously what I was using:
-        //px = 0 - f[p-w-1] + f[p-w+1] - (f[p-1]<<1) + (f[p+1]<<1) - f[p+w-1] + f[p+w+1];
-        
-        py =  n31 + (n32 << 1) + n33 - (n11 + (n12 << 1) + n13);
-        //py = f[p-w-1] + (f[p-w]<<1) + f[p-w+1] - ( f[p+w-1] + (f[p+w]<<1) + f[p+w+1] );
+        int px = n13 + (n23 << 1) + n33 - (n11 + (n21 << 1) + n31);
+        int py = n31 + (n32 << 1) + n33 - (n11 + (n12 << 1) + n13);
         
         if(px<0) px=-px; 
         if(py<0) py=-py;
-        ps=px+py; 
-        //if(ps>95) ps=255; if(ps<=95) ps=0;
-        //dbuf[p] = (ps<<24)|(ps<<16)|(ps<<8)|ps;
+        int ps = px+py;
         g[p] = ps > 95 ? WHITE : TRANSPARENT; 
     }
-    /*
-    #pragma omp parallel for default(shared) private(y)
-	for(y=1; y<maxy; y++)//, p+=2)
-	{
-        const int row = y * width;
-        int x, px, py, ps;
-	
-        for(x=1; x<maxx; x++)//, p++)
-		{
-            const int p = row + x;
-            
-            px = n13 + (n23 << 1) + n33 - (n11 + (n21 << 1) + n31);
-            //previously what I was using:
-            //px = 0 - f[p-w-1] + f[p-w+1] - (f[p-1]<<1) + (f[p+1]<<1) - f[p+w-1] + f[p+w+1];
-			
-            py =  n31 + (n32 << 1) + n33 - (n11 + (n12 << 1) + n13);
-            //py = f[p-w-1] + (f[p-w]<<1) + f[p-w+1] - ( f[p+w-1] + (f[p+w]<<1) + f[p+w+1] );
-            
-        	if(px<0) px=-px; 
-            if(py<0) py=-py;
-			ps=px+py; 
-            //if(ps>95) ps=255; if(ps<=95) ps=0;
-			//dbuf[p] = (ps<<24)|(ps<<16)|(ps<<8)|ps;
-            g[p] = ps > 95 ? WHITE : TRANSPARENT; 
-		}
-	}
-    */
+    
     (*env)->ReleaseByteArrayElements(env, frame, f, JNI_COMMIT);
 }
