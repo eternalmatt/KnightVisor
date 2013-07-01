2ac9c24efb53f1425bb353d5ca2a783cf7868828
eternalmatt <eternalmatt@gmail.com>
2013-06-30 22:11:10 -0400

    Added basic implementation for filterscript

--- Android/src/com/visor/knight/FilterscriptConverter.java
+++ Android/src/com/visor/knight/FilterscriptConverter.java
@@ -1,6 +1,10 @@
 package com.visor.knight;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
 
 public class FilterscriptConverter extends EdgeConverter {
 
@@ -19,13 +23,30 @@ public class FilterscriptConverter extends EdgeConverter {
 	public void setSoftEdges(boolean softEdges) {
 	}
 	
-	public FilterscriptConverter(int width, int height) {
+	private final ScriptC_filter mScript = null;
+	private final Allocation inAllocation;
+	private final Allocation outAllocation;
+	private final Bitmap outBitmap;
+	
+	public FilterscriptConverter(Context ctx, int width, int height) {
 		super(width, height);
-		
+		/* potentially correct declarations */
+		RenderScript rs = RenderScript.create(ctx);
+		inAllocation  = Allocation.createSized(rs, Element.RGBA_8888(rs), width*height, Allocation.USAGE_SCRIPT);
+		outAllocation = Allocation.createSized(rs, Element.RGBA_8888(rs), width*height, Allocation.USAGE_SCRIPT);
+		mScript.set_in(inAllocation);
+		mScript.set_width(width);
+		mScript.set_height(height);
+		outBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+
 	}
+	
 	public Bitmap convertFrame(byte[] yuvFrame) {
+		/* potentially correct implementation */
+		inAllocation.copy2DRangeFrom(0, 0, width, height, yuvFrame);
+		mScript.forEach_root(outAllocation);
+		outAllocation.copyTo(outBitmap);
 
-		return null;
+		return outBitmap;
 	}
-
 }
