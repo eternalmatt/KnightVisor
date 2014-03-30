8f4b8d265c57040df6e83e2211bf6da394f07274
Matt Senn <eternalmatt@gmail.com>
2014-03-30 14:44:34 -0400

    Stable filterscript implementation

--- Android/src/com/visor/knight/converter/FilterscriptConverter.java
+++ Android/src/com/visor/knight/converter/FilterscriptConverter.java
@@ -2,8 +2,13 @@ package com.visor.knight.converter;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.ImageFormat;
 import android.renderscript.Allocation;
+import android.renderscript.Element;
 import android.renderscript.RenderScript;
+import android.renderscript.ScriptGroup;
+import android.renderscript.ScriptIntrinsicYuvToRGB;
+import android.renderscript.Type;
 
 import com.visor.knight.R;
 import com.visor.knight.ScriptC_filter;
@@ -26,32 +31,59 @@ public class FilterscriptConverter extends EdgeConverter {
 	}
 	
 	private final RenderScript rs;
-	private final ScriptC_filter mScript;
-	private Allocation inAllocation;
-	private Allocation outAllocation;
-	private Bitmap outBitmap;
+	private final ScriptC_filter script;
+	private final ScriptIntrinsicYuvToRGB yuv;
+	private ScriptGroup scriptGroup;
+	private Allocation in;
+	private Allocation out;
+	private Bitmap bitmap;
 	
 	public FilterscriptConverter(Context ctx){
 		this.rs = RenderScript.create(ctx);
-		mScript = new ScriptC_filter(rs, ctx.getResources(), R.raw.filter);
+		script = new ScriptC_filter(rs, ctx.getResources(), R.raw.filter);
+        yuv = ScriptIntrinsicYuvToRGB.create(rs, Element.RGBA_8888(rs));
 	}
 	
 	@Override
 	protected void initialize(int width, int height) {;
-		outBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-		inAllocation  = Allocation.createFromBitmap(rs, outBitmap);
-		outAllocation = Allocation.createFromBitmap(rs, outBitmap);
-		mScript.set_in(inAllocation);
-		mScript.set_width(width);
-		mScript.set_height(height);
+    	bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+    	script.set_width(width);
+    	script.set_height(height);
+
+    	Type.Builder tb = new Type.Builder(rs, Element.RGBA_8888(rs));
+	    tb.setX(width);
+	    tb.setY(height);
+	    Type t = tb.create();
+	    out = Allocation.createTyped(rs, t, Allocation.USAGE_SCRIPT);// | Allocation.USAGE_IO_OUTPUT);
+	    
+	    tb = new Type.Builder(rs, Element.createPixel(rs, Element.DataType.UNSIGNED_8, Element.DataKind.PIXEL_YUV));
+        tb.setX(width);
+        tb.setY(height);
+        tb.setYuvFormat(ImageFormat.NV21);
+        in  = Allocation.createTyped(rs, tb.create(), Allocation.USAGE_SCRIPT);
+	    
+        ScriptGroup.Builder b = new ScriptGroup.Builder(rs);
+        b.addKernel(script.getKernelID_sobel());
+        b.addKernel(yuv.getKernelID());
+        b.addConnection(t, yuv.getKernelID(), script.getFieldID_in());
+        scriptGroup = b.create();
 	}
 	
-	public Bitmap convertFrame(byte[] yuvFrame) {
-		/* potentially correct implementation */
-		inAllocation.copy2DRangeFrom(0, 0, width, height, yuvFrame);
-		mScript.forEach_root(outAllocation);
-		outAllocation.copyTo(outBitmap);
+	@Override
+	public Bitmap convertFrame(final byte[] yuvFrame) {
 
-		return outBitmap;
+	    in.copyFrom(yuvFrame);
+        yuv.setInput(in);
+	    scriptGroup.setOutput(script.getKernelID_sobel(), out);
+	    scriptGroup.execute();
+	    out.copyTo(bitmap);
+	    
+		/* potentially correct implementation 
+		in.copyFrom(yuvFrame);
+		script.forEach_sobel(out);
+		out.copyTo(bitmap);
+		*/
+	    
+		return bitmap;
 	}
 }
--- Android/src/com/visor/knight/converter/filter.fs
+++ Android/src/com/visor/knight/converter/filter.fs
@@ -1,12 +1,90 @@
 #pragma version(1)
 #pragma rs java_package_name(com.visor.knight)
 
+typedef int32_t  number;
+typedef uchar4   point;
+typedef int32_t  pixel;
+
 rs_allocation in;
 
-uint32_t width;
-uint32_t height;
+number width = 0;
+number height = 0;
 
-uchar4 __attribute__((kernel)) root(uint32_t x, uint32_t y) {
-   //lots of todos!
-	return 0;
+static inline bool outOfBounds(const number x, const number y) {
+	const number b = 1;
+	return x-b <= 0 || y-b <= 0 || x+b >= width || y+b >= height;
+}
+
+static inline pixel getElementAt(const number x, const number y) {
+	point p = rsGetElementAt_uchar4(in, x, y);
+	return ((p.x + p.y + p.z ) / 3) & 0xFF; //average and restrict to 255
+}
+
+point __attribute__((kernel)) sobel(const uint32_t x, const uint32_t y) {
+
+	if (outOfBounds(x,y)){
+		//rsDebug("out of bounds", x, y, width, height);
+		pixel p = getElementAt(x,y);
+		point po = {p,p,p,255};
+		return po;
+	} 
+
+	const pixel
+		f11 = getElementAt(x-1,y-1)
+	  , f12 = getElementAt(x-1,y  )
+	  , f13 = getElementAt(x-1,y+1)
+	  , f21 = getElementAt(x  ,y-1)
+	  , f22 = getElementAt(x  ,y  )
+	  , f23 = getElementAt(x  ,y+1)
+	  , f31 = getElementAt(x+1,y-1)
+	  , f32 = getElementAt(x+1,y  )
+	  , f33 = getElementAt(x+1,y+1)
+	  ;
+
+	const pixel
+	    gx = (f13 + (f23 << 1) + f33) - (f11 + (f21 << 1) + f31)
+      , gy = (f11 + (f12 << 1) + f13) - (f31 + (f32 << 1) + f33)
+	  , gm = (gx + gy) / 2
+	  ;
+    
+    if (gm > 100) {
+    	point p = { 0, gm, 0, 0xff };
+    	return p;
+    } else {
+    	return rsGetElementAt_uchar4(in, x, y);
+    }
+}
+
+point __attribute__((kernel)) median(uint32_t x, uint32_t y) {
+	if (outOfBounds(x,y)) {
+		return rsGetElementAt_uchar4(in, x, y);
+	}
+	#define MAXHIST (8)
+	const pixel
+		f11 = getElementAt(x-1,y-1)
+	  , f12 = getElementAt(x-1,y  )
+	  , f13 = getElementAt(x-1,y+1)
+	  , f21 = getElementAt(x  ,y-1)
+	  , f22 = getElementAt(x  ,y  )
+	  , f23 = getElementAt(x  ,y+1)
+	  , f31 = getElementAt(x+1,y-1)
+	  , f32 = getElementAt(x+1,y  )
+	  , f33 = getElementAt(x+1,y+1)
+	  ;
+	
+	const pixel f[9] = { f11, f12, f13, f21, f22, f23, f31, f32, f33 };
+	int i, sum, hist[MAXHIST] = {0,0,0,0,0,0,0,0}; 
+	for(i = 0; i < 9; i++)
+	{
+		int p = f[i] / 32;
+		hist[ p < MAXHIST ? p : MAXHIST - 1]++;
+	}
+	
+	sum = hist[0];
+	for(i = 1; sum < 5; i++){
+		sum += hist[i];
+	}
+	
+	uint p = f[i];
+	return convert_uchar4((uint4){p,p,p,255});
 }
